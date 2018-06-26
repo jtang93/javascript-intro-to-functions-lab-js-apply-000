dc91af429b57303bbc1251fa5cfe69b51e18df06
jtang93 <tang.jonathan93@gmail.com>
2018-06-26 18:39:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,15 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-  return('I can\'t hear you!')}
-
-  else (string.toUpperCase() === string) {
-    return('YES INDEED!')
-  }
-}
-
-function sayHiToGrandma(string) {
-  if (string === 'I love you, Grandma.') {
+   return('I can\'t hear you!')}
+  else if (string.toUpperCase() === string) {
+    return('YES INDEED!')}
+  else if (string === 'I love you, Grandma.') {
     return('I love you, too.')
   }
 }
