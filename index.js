2464711b7302a41275b8f1a84d2b2fecbd351e94
jtang93 <tang.jonathan93@gmail.com>
2018-06-26 18:36:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toUpperCase() === string {
+  if (string.toUpperCase() === string) {
     return('YES INDEED!')
   })
 }
